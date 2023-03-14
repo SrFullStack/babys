a3559afd47f57794f98a6b77dbe4a42c94787722
rivki <325303980@PC3-4>
2023-03-14 18:31:03 +0200

    14.03

--- src/GetallBaby.js
+++ src/GetallBaby.js
@@ -4,33 +4,69 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 export default function GetallBaby() {
 
-    const [employee, setEmployee] = useState({
-        name: '',
-        salary: 0,
-        times:[
-            {
-                shhs:''
-            }
-        ],
-      });
-const [babySitters,setBabySitters]= useState({BabysiterId:0,FirstName:"",LastName:"",Age:0,Email:"",Password:"",Description:"",Phone:""
-,times:[{TimeId:0,BabysiterId:0,Day:"",PartOfDay:"",Price:0}],NeighborhoodBabysiter:[{NeighborhoodBabysiterId:0,BabysiterId:0,NeighborhoodId:0}]})
+    const [search, setSearch] = useState({ typeId: -1, categoryId: -1, subCategoryId: -1, datePublished: null, startDate: null, endDate: null, common: "defaultValue" })
+const [babySitters, setBabySitters] = useState([{BabysiterId:-1,FirstName:"",LastName:"",Age:0,Email:"",Password:"",Description:"",Phone:""
+,times:[{TimeId:0,BabysiterId:0,Day:"",PartOfDay:"",Price:0}],NeighborhoodBabysiter:[{NeighborhoodBabysiterId:0,BabysiterId:0,NeighborhoodId:0}]}])
 const [baby,setbaby]=useState([])
 
+
 useEffect(() => {
+   
+    // setBabySitters({...babySitters,"BabysiterId":5})
     try{
-        var w;
-      
+        const updateUsers=[];
+        var baby=[];
+        var time=[];
+        var Neighborhood=[];
          axios.get(`https://localhost:44312/api/BabySiter/GetAll`)
            .then(response =>{
      
-w=response.data;
-w.forEach(w => {
-    setbaby([...baby,{BabysiterId:w.babysiterId,FirstName:w.firstName,LastName:w.lastName,Age:w.age,Email:w.email,Password:w.password,Description:w.description,Phone:w.phone}]);
-    console.log(baby);  
-    })
+baby=response.data;
+// response.data.forEach(w => {
+//     setbaby([...baby,10])
+//     console.log(baby);  
+//     })
+// setbaby([...baby,10])
+// setbaby([...baby,10])
 
-    console.log(baby);  
+//     console.log(baby); 
+
+
+  
+
+
+for (let index = 0; index < response.data.length; index++) {
+   let id=response.data[index].babysiterId
+
+    // setBabySitters({...babySitters,["BabysiterId"]:id})
+
+
+      
+console.log(babySitters);
+    axios.get(`https://localhost:44312/api/Time/Get?BabysiterId=${id}`)
+    .then(response =>{
+
+time=response.data;}) 
+axios.get(`https://localhost:44312/api/NeighborhoodBabysiter/Get?id=${id}`)
+.then(response =>{
+
+Neighborhood=response.data;})   
+setBabySitters( [
+    ...babySitters,
+    {
+        BabysiterId: baby[index].babysiterId,
+        FirstName: baby[index].firstName,
+        LastName:baby[index].lastName,
+        Age:baby[index].age,
+        Description:baby[index].description,
+typeId:56,
+    }
+    
+  ]);
+}
+   
+
+
 });
 
             
@@ -45,6 +81,6 @@ w.forEach(w => {
  
     return (<div>
 
-
+{/* <button onClick={t}>y</button> */}
     </div>);
 };
