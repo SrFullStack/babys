65a4e594a5207e5a2b934a47915bd027c57cb04d
SrFullStack <rsgithubb@gmail.com>
2023-05-24 02:36:28 +0300

    24/05/2023

--- src/Menu.js
+++ src/Menu.js
@@ -1,8 +1,6 @@
 
 import { useState, useEffect } from "react";
-
 import React from 'react';
-
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -61,7 +59,77 @@ catch (err) {
     console.error(err);
 }
 }
-  const babysiter = () => {
+
+const [email, setEmail] = useState("");
+const [babysiter, setbabysiter] = useState("");
+
+
+
+const UpdateOpinion =async ()=>{
+    try{
+      await axios.get(`https://localhost:44312/api/BabySiter/GetByEmail?Email=${email}`)
+         .then(async response =>{
+            if(response.data.babysiterId!=undefined)
+         {
+        
+        var age =response.data.age+1;
+           const babysiter=({babysiterId:response.data.babysiterId,FirstName:response.data.firstName,LastName:response.data.lastName,Phone:response.data.phone,Age:age, Email:response.data.email,Password:response.data.password,Description:response.data.description});
+           AddOpinion(babysiter);
+        }
+         else{
+            alert("אין כזה ביביסיטר נסה שוב") 
+         }
+           
+         })
+       
+     }        
+     
+     catch (err) {
+         console.error(err);
+     }
+ }
+ const AddOpinion =async (babysiter)=>{
+    try{                           
+        await axios.put(`https://localhost:44312/api/BabySiter/Put?id=${babysiter.babysiterId}`,babysiter)
+        .then(response =>{ 
+            alert("תודה רבה על ההמלצה החמה") 
+            EmailForBenefits(babysiter);
+      })
+      }
+      catch(err){
+         console.log(err);
+      }
+ }
+
+ const EmailForBenefits=async (babysiter)=>{
+if(babysiter.Age>10){
+    axios.get( `https://localhost:44312/api/RequsetSearchBabysiter/GetEmail?email=${email}`)
+        .then(res => {
+            const data1 = res.data;
+            ResetBenefits(babysiter)
+            alert("נשלח מייל")
+        })
+    }
+    else{
+        alert("hhh")
+    }
+    }
+ 
+
+ const ResetBenefits =async (babysiter)=>{       
+        try{  
+            var babysiterResert=({babysiterId:babysiter.babysiterId,FirstName:babysiter.FirstName,LastName:babysiter.LastName,Phone:babysiter.Phone,Age:0, Email:babysiter.Email,Password:babysiter.Password,Description:babysiter.Description});                         
+            await axios.put(`https://localhost:44312/api/BabySiter/Put?id=${babysiter.babysiterId}`,babysiterResert)
+            .then(response =>{ 
+               
+          })
+          }
+          catch(err){
+             console.log(err);
+          }
+     
+     }
+  const babysiterr = () => {
    
     navigate("/BabySiterGetById", { replace: false })
   }
@@ -88,7 +156,7 @@ catch (err) {
          })
         } */}
    <div id="towbt">
-   <button id="btbabisiter" onClick={babysiter}>חיפוש עבודה      </button>
+   <button id="btbabisiter" onClick={babysiterr}>חיפוש עבודה      </button>
     <button id="btsearchbabysiter"onClick={searchBabySiter}>חיפוש מועמדים</button>
     <button onClick={RequsetSearchBabysiterr}>חיפוש RequsetSearchBabysiterr</button>
 
@@ -112,6 +180,8 @@ catch (err) {
     <PM id="PM"></PM>
     <V id="V"></V>
     <TM id="TM"></TM>
+    <input id="inputemail" className="input"  placeholder='email' onChange={(e) => setEmail(e.target.value)} /><br></br>
+   <button onClick={UpdateOpinion}>updateOpinion</button>
    </div>
     </div>);
 };
